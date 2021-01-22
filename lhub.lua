e1c1dba627ec9b88b7b9cebee70b036a65ff7457
dacadla <71786326+dacadla@users.noreply.github.com>
2021-01-22 18:39:39 +0100

    Update lhub.lua

--- lhub.lua
+++ lhub.lua
@@ -26,6 +26,8 @@ hub.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 hub.Position = UDim2.new(0.468980789, 0, 0.363103956, 0)
 hub.Size = UDim2.new(0, 266, 0, 182)
 hub.Visible = false
+hub.Active=true
+hub.Draggable=true
 
 title.Name = "title"
 title.Parent = hub
